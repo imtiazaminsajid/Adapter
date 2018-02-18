85e57fe7b1f36a6c9b9a9de10fd02ff2dcf003d8
imtiazaminsajid <ambition.sajid@gmail.com>
2018-02-18 22:43:50 +0600

    commited

--- app/src/main/java/com/example/imtiazaminsajid/adapter/MainActivity.java
+++ app/src/main/java/com/example/imtiazaminsajid/adapter/MainActivity.java
@@ -4,16 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    private ListView listView;
+    private ListView mylistView;
+    private ArrayList<ContactPerson> persons;
+    private ContactPerson contactPerson;
+    private PersonAdapter PersonAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.listView);
-        
+        mylistView = findViewById(R.id.listView);
+        contactPerson = new ContactPerson();
+        persons = contactPerson.getAllPersons();
+        PersonAdapter = new PersonAdapter(this,persons);
+        mylistView.setAdapter(PersonAdapter);
+
+
+
     }
 }
